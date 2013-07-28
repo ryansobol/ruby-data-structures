e11c916f14a5fb79d1ae74dc8287c0db95dcf115
Ryan Sobol <ryansobol@gmail.com>
2013-07-28 13:22:55 -0500

    Add Big O notation

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,18 @@
 class Node
-  attr_accessor :value
-  attr_reader :prev, :next
-
+  # O(1)
   def initialize(value, prevv = nil, nexxt = nil)
     @value    = value
     self.prev = prevv
     self.next = nexxt
   end
 
+  # O(1)
+  attr_accessor :value
+
+  # O(1)
+  attr_reader :prev, :next
+
+  # O(1)
   def next=(nexxt)
     unless nexxt.nil? || nexxt.is_a?(self.class)
       raise "next must be nil or a #{self.class} instance"
@@ -16,6 +21,7 @@ class Node
     @next = nexxt
   end
 
+  # O(1)
   def prev=(prevv)
     unless prevv.nil? || prevv.is_a?(self.class)
       raise "prev must be nil or a #{self.class} instance"
@@ -24,12 +30,14 @@ class Node
     @prev = prevv
   end
 
+  # O(1)
   def clear
     temp = value
     self.value = self.next = self.prev = nil
     temp
   end
 
+  # O(1)
   def ==(other)
     return false unless other
     self.value == other.value && self.prev == other.prev && self.next == other.next
