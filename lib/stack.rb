1d4e5b72c259234ef1604736a5afbcf057655fbc
Ryan Sobol <contact@ryansobol.com>
2014-11-06 07:33:25 -0800

    Add Stack class

---
+++ lib/stack.rb
@@ -0,0 +1,44 @@
+class Stack
+  # O(1)
+  def initialize
+    @size = 0
+    @data = []
+  end
+
+  # O(1)
+  attr_reader :size
+
+  # O(1)
+  attr_reader :data
+  private :data
+
+  # O(1)
+  def empty?
+    size.zero?
+  end
+
+  # O(1)
+  def peek
+    data[size]
+  end
+
+  # O(1)
+  def push(value)
+    data[size] = value
+    @size += 1
+    value
+  end
+
+  # O(1)
+  def pop
+    @size -= 1
+    temp = data[size]
+    data[size] = nil
+    temp
+  end
+
+  # O(1)
+  def at(index)
+    data[index]
+  end
+end
